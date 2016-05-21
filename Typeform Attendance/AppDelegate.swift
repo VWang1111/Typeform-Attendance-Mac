d227a2984504d15f6d0752fbbf6a3c07f19eb832
Victor <v4wang@ucsd.edu>
2016-05-20 20:20:08 -0700

    Initial Commit

---
+++ Typeform Attendance/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Typeform Attendance
+//
+//  Created by Victor Wang on 5/20/16.
+//  Copyright © 2016 Victor Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
